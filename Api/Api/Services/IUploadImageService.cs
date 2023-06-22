87f6b1c19f1e373512f3bd394395c3685b93f72a
renko7 <sdgsdfgsdfg>
2023-06-21 23:31:29 -0500

    creating service for uploading images

---
+++ Api/Api/Services/IUploadImageService.cs
@@ -0,0 +1,9 @@
+﻿using Azure.Storage.Blobs.Models;
+
+namespace MyInventory.Api.Services;
+
+public interface IUploadImageService
+{
+    // change from blobcontentinfo to upload result to not tie to azure
+    Task<BlobContentInfo> UploadImage(string name, IFormFile image, Dictionary<string, string>? customMetadata = null);
+}
---
+++ Api/Api/Services/UploadImageService.cs
@@ -0,0 +1,34 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.Extensions.Options;
+using MyInventory.Api.Models;
+
+namespace MyInventory.Api.Services;
+
+public class UploadImageService : IUploadImageService
+{
+    private readonly AppSettings _appSettings;
+
+    public UploadImageService(IOptions<AppSettings> options)
+    {
+      _appSettings = options.Value;
+    }
+
+    public async Task<BlobContentInfo> UploadImage(string name, IFormFile image, Dictionary<string, string>? customMetadata = null)
+    {
+        BlobServiceClient blobServiceClient = new BlobServiceClient(_appSettings.StorageAccountConnectionString);
+        BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(_appSettings.StorageAccountContainerName);
+        BlobClient blobClient = containerClient.GetBlobClient(name);
+
+        var memoryStream = new MemoryStream();
+        await image.CopyToAsync(memoryStream);
+        memoryStream.Position = 0;
+
+
+        var result = await blobClient.UploadAsync(memoryStream);
+        
+        var response = await blobClient.SetMetadataAsync(customMetadata);
+
+        return result;
+    }
+}
