ef4b8c527f29823348dee1d48abfa6aa72469ab3
renko7 <sdgsdfgsdfg>
2023-06-24 19:38:26 -0500

    taking steps for github actions

---
+++ ImagesProcessor/ImagesProcessor/ImageAddedProcessor.cs
@@ -0,0 +1,29 @@
+// Default URL for triggering event grid function in the local environment.
+// http://localhost:7071/runtime/webhooks/EventGrid?functionName={functionname}
+using System;
+using Azure.Messaging;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace MyInventory.ImagesProcessor
+{
+    public class ImageAddedProcessor
+    {
+        private readonly ILogger _logger;
+
+        public ImageAddedProcessor(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ImageAddedProcessor>();
+        }
+
+        [Function("ImageAddedProcessor")]
+        public void Run([EventGridTrigger] CloudEvent imageAddedEvent)
+        {
+            var serializedInput = imageAddedEvent.ToString();
+
+            _logger.LogInformation(serializedInput);
+
+            Console.WriteLine(serializedInput);
+        }
+    }
+}
